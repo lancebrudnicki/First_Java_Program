02f3a4519ee1395c3dcbdb153ec475a1320cf6da
lancebrudnicki <lancebrudnicki@gmail.com>
2021-07-31 15:18:35 -0600

    2021 july

---
+++ FirstJAvaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Lance");
+        System.out.println("I am 29123 years old");
+        System.out.println("MY hometown is Georgia");
+    }
+}
